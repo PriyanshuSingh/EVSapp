295239a19c2a8430c0af3085462e43a8363471ef
Siddharth Jain <jain.siddharth123@gmail.com>
2015-03-07 16:11:52 +0530

    Dynamic Menu
    jsoup part commented out

--- app/src/main/java/evsapp/sid/com/evsapp/MainActivity.java
+++ app/src/main/java/evsapp/sid/com/evsapp/MainActivity.java
@@ -5,39 +5,109 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static android.widget.AdapterView.OnItemSelectedListener;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements OnItemSelectedListener {
 
     TextView display;
+    Spinner stateList,cityList;
+    ArrayAdapter<CharSequence> mStateArrayAdapter=null,mCityArrayAdapter=null;
+    ArrayList<String> cities = null;
+    ArrayList<String> delhi = null;
+    ArrayList<String> uttar_pradesh = null;
+    ArrayList<String> andhra_pradesh = null;
+    ArrayList<String> bihar = null;
+    ArrayList<String> gujarat = null;
+    ArrayList<String> haryana = null;
+    ArrayList<String> karnataka = null;
+    ArrayList<String> maharashtra = null;
+    ArrayList<String> tamil_nadu = null;
 
-    private class ReadWebpageContents extends AsyncTask<String,Void,Document> {
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+        display = (TextView)findViewById(R.id.resultDisplay);
+        switch(parent.getId()){
+            case R.id.states_list: display.setText("state selected");setCityList(position);break;
+
+            case R.id.city_list: display.setText("city selected");break;
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    private void setCityList(int position){
+
+        cities.clear();
+        switch(position){
+            case 0: cities.addAll(andhra_pradesh);break;
+            case 1: cities.addAll(bihar);break;
+            case 2: cities.addAll(delhi);break;
+            case 3: cities.addAll(gujarat);break;
+            case 4: cities.addAll(haryana);break;
+            case 5: cities.addAll(karnataka);break;
+            case 6: cities.addAll(maharashtra);break;
+            case 7: cities.addAll(tamil_nadu);break;
+            case 8: cities.addAll(uttar_pradesh);/*mCityArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,uttar_pradesh);*/break;
+        }
+
+        //mCityArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mCityArrayAdapter.notifyDataSetChanged();
+    }
+    private class ReadWebpageContents extends AsyncTask<String, Void, Void> {
         @Override
-        protected Document doInBackground(String... params) {
+        protected Void doInBackground(String... params) {
             try {
                 Document doc = Jsoup.connect(params[0]).get();
+                display.setText(doc.text());
+                //printTableContent(doc);
                 //display.setText(doc.title());
-                return doc;
+                //return doc;
             } catch (IOException e) {
                 e.printStackTrace();
             }
             return null;
         }
 
-        @Override
-        protected void onPostExecute(Document document){
-            if(document!=null)
-            display.setText(document.title());      //PRINTING THE TITLE OF THE WEBPAGE
-            else
-                display.setText("null");
+        private void printTableContent(Document doc){
+            Element mTable = null;
+            Elements mRows=null;
+
+            //if((mTable=doc.getElementById("Td1"))==null)
+            //    display.append("mtable null");
+
+            /*if((mRows = mTable.select("tr"))==null)
+                display.append("mrows null");
+            for(int i=1; i < mRows.size(); i++){
+                Element mRow = mRows.get(i);
+                Elements mCols = mRow.select("td");
+                display.append(mRow.text()+"\n");
+                for(int j=0; j<mCols.size(); i++){
+                     display.append(mCols.get(j).text()+"\t");
+                }
+            }
+            */
         }
     }
 
@@ -46,11 +116,40 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         display = (TextView)findViewById(R.id.resultDisplay);
-        String website = "http://www.cpcb.gov.in/CAAQM/mapPage/frmindiamap.aspx";  //HOMEPAGE OF THE WEBSITE
+
+        cities = new ArrayList<>();
+        delhi = new ArrayList<>(Arrays.asList("D.C.E.","Shadipur","I.T.O.","Dilshad Garden","Dwarka"));
+        uttar_pradesh = new ArrayList<>(Arrays.asList("Agra","Lucknow","Kanpur","Varanasi"));
+        andhra_pradesh = new ArrayList<>(Arrays.asList("Hyderabad"));
+        bihar = new ArrayList<>(Arrays.asList("Patna"));
+        gujarat = new ArrayList<>(Arrays.asList("Ahmedabad"));
+        haryana = new ArrayList<>(Arrays.asList("Faridabad"));
+        karnataka = new ArrayList<>(Arrays.asList("Bangalore"));
+        maharashtra = new ArrayList<>(Arrays.asList("Mumbai"));
+        tamil_nadu = new ArrayList<>(Arrays.asList("Chennai"));
+
+        stateList = (Spinner)findViewById(R.id.states_list);
+
+        cityList = (Spinner)findViewById(R.id.city_list);
+        mStateArrayAdapter = ArrayAdapter.createFromResource(this,R.array.states_list,android.R.layout.simple_spinner_item);
+        mStateArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        stateList.setAdapter(mStateArrayAdapter);
+
+        cities.addAll(delhi);
+        mCityArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,cities);
+
+
+        mCityArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cityList.setAdapter(mCityArrayAdapter);
+        stateList.setOnItemSelectedListener(this);
+        cityList.setOnItemSelectedListener(this);
+
+        String base = "http://www.cpcb.gov.in/CAAQM/frmCurrentDataNew.aspx?StationName=";
+        String website = "http://www.cpcb.gov.in/CAAQM/frmCurrentDataNew.aspx?StationName=D.C.E.&StateId=6&CityId=85";  //HOMEPAGE OF THE WEBSITE
         ReadWebpageContents mReadWebpageContents = new ReadWebpageContents();
         //try {
             //URL mUrl = new URL(website);
-            mReadWebpageContents.execute(website);
+           //mReadWebpageContents.execute(website);
        /* } catch (MalformedURLException e) {
             e.printStackTrace();
         }*/
